60a933a1754a62c0efecc4a42986ce577623f21c
Nana Boateng <44186742+nana-boateng@users.noreply.github.com>
2022-01-23 21:21:08 -0500

    Finish api setup, now awaiting frontend layout from Josh

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,2 +1,8 @@
-import { FC } from "react";
-import { useQuery, UseQueryResult } from "react-query";
+import axios from "axios";
+
+export const fetchAssignments = async (id?: Number) => {
+  const { data } = await axios.get(
+    `https://cisc322-website.herokuapp.com/api/assignments/${id ? id : ""}`
+  );
+  return data;
+};
