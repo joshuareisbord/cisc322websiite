34a19c9324df8751d81f13ddf647868f2d063ef3
joshuareisbord <joshuareisbord@gmail.com>
2022-01-26 13:28:26 -0500

    ben wanted a different pepe

--- src/constants/team_members.ts
+++ src/constants/team_members.ts
@@ -2,7 +2,7 @@ export const TEAM_MEMBERS = [
 
     {img: 'https://static01.nyt.com/images/2016/09/28/us/17xp-pepethefrog_web1/28xp-pepefrog-articleLarge.jpg?quality=75&auto=webp&disable=upscale', name: 'Aarti Singh'},
     {img: 'https://wgbh.brightspotcdn.com/dims4/default/7e333a8/2147483647/strip/true/crop/1000x545+0+30/resize/990x540!/format/jpg/quality/70/?url=http%3A%2F%2Fcdn-news.wgbh.org%2Fs3fs-public%2FCrying-Frog-Meme-06.jpg', name: 'Aubrey McLeod'},
-    {img: 'https://media-cldnry.s-nbcnews.com/image/upload/t_focal-760x428,f_auto,q_auto:best/MSNBC/Components/Video/201609/a_ov_Pepe_160928.jpg', name: 'Benjamin Durston'},
+    {img: 'https://i.redd.it/9e9psp36nwg51.jpg', name: 'Benjamin Durston'},
     {img: 'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy81ODc5Mzc2L29yaWdpbi5qcGciLCJleHBpcmVzX2F0IjoxNzAzOTU5MjAxfQ.yr8Tz4vE0GBqSDQqDVHqbrs8umNAhVAQjQmBvCDILfA/img.jpg?width=824&quality=80', name: 'Joshua Reisbord'},
     {img: 'https://i.guim.co.uk/img/media/327e46c3ab049358fad80575146be9e0e65686e7/0_0_1023_742/master/1023.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=1feb1959e48be497be6ed215da0aa3bd', name: 'Nana Boateng'},
     {img: 'https://cdn.vox-cdn.com/thumbor/7ngltsNfAD61P_XbIYxEq17NsPs=/0x0:778x778/920x0/filters:focal(0x0:778x778):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/10838149/poggers.jpg', name: 'Tamir Arnesty'},
